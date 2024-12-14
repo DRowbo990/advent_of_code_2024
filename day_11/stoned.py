e3b7415f07759c31e7c8949ac33e6a20c4d7d042
DRowbo990 <davidrowbotham98@gmail.com>
2024-12-13 22:18:35 -0700

    smart part 2 I want to learn more about

--- day_11/stoned.py
+++ day_11/stoned.py
@@ -1,42 +1,25 @@
-import sys  # Import the sys module to access command-line arguments
+stones = list(map(int, open("input.txt").readline().strip().split()))
 
-# Check if the correct number of arguments is provided
-if len(sys.argv) != 2:
-    print(
-        "Usage: python3 stoned.py <inputfile>"
-    )  # Print usage message if incorrect arguments
-    sys.exit(1)  # Exit the program with an error code
-
-# Get the input file from the command line arguments
-input_file = sys.argv[1]
-
-# Read the input file and store each character in a 2D array
-stoned = []
-with open(input_file, "r") as file:  # Open the input file for reading
-    for line in file:  # Iterate over each line in the file
-        elements = line.strip().split()  # Split the line into individual elements
-        stoned.extend(elements)  # Append each element to the list
-
-# Convert each element to an integer
-stoned = [int(element) for element in stoned]
+memory = {}
 
 
-def process_stones(stones):
-    for element in stones:
-        if element == 0:
-            yield 1
-        elif len(str(element)) % 2 == 0:
-            element_str = str(element)
-            mid = len(element_str) // 2
-            left = element_str[:mid].lstrip("0")
-            right = element_str[mid:].lstrip("0")
-            yield int(left) if left else 0
-            yield int(right) if right else 0
-        else:
-            yield element * 2024
+def solve(stone, blinks):
+    if blinks == 0:
+        return 1
+    elif (stone, blinks) in memory:
+        return memory[(stone, blinks)]
+    elif stone == 0:
+        val = solve(1, blinks - 1)
+    elif len(str_stone := str(stone)) % 2 == 0:
+        mid = len(str_stone) // 2
+        val = solve(int(str_stone[:mid]), blinks - 1) + solve(
+            int(str_stone[mid:]), blinks - 1
+        )
+    else:
+        val = solve(stone * 2024, blinks - 1)
+    memory[(stone, blinks)] = val
+    return val
 
 
-for _ in range(5):
-    stoned = list(process_stones(stoned))
-
-print(len(stoned))
+print(sum(solve(stone, 25) for stone in stones))
+print(sum(solve(stone, 75) for stone in stones))
